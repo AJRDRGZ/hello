0e55ff540f015f8d0c52d4c7df00a3d27f12a282
Alejandro Rodriguez <aj.softwaredeveloper@gmail.com>
2020-03-10 14:24:50 -0500

    Initial commit

---
+++ greet/greet.go
@@ -0,0 +1,13 @@
+package greet
+
+var emoji = "🙋‍"
+
+// English retorna saludo en ingles
+func English() string {
+	return "Hi " + emoji
+}
+
+// Italian retorna saludo en italiano
+func Italian() string {
+	return "Ciao " + emoji
+}
---
+++ greet/spanish.go
@@ -0,0 +1,6 @@
+package greet
+
+// Spanish retorna saludo en español
+func Spanish() string {
+	return "Hola " + emoji
+}

8239802b29eb31a729abb2a24f0646f836ccc41c
Alejandro Rodriguez <aj.softwaredeveloper@gmail.com>
2020-03-12 07:34:43 -0500

    End of course

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/AJRDRGZ/hello/greet"
+	"rsc.io/quote/v3"
 )
 
 func main() {
-	fmt.Println("Hello")
+	fmt.Println(greet.Italian())
+	fmt.Println(quote.HelloV3())
+	fmt.Println(quote.Concurrency())
 }
